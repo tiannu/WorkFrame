621a25e41692139f1d5856e246a5ba4023840581
tiannu <tian@tiannutekiMacBook-Pro-2.local>
2015-09-23 21:49:56 +0800

    create project

---
+++ WorkFrame/WorkFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorkFrame
+//
+//  Created by tiannu on 15/9/22.
+//  Copyright (c) 2015年 tiannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorkFrame/WorkFrame/UI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorkFrame
+//
+//  Created by tiannu on 15/9/22.
+//  Copyright (c) 2015年 tiannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

9db8ef8de5e17a117b9c24511376c6b650838a4a
haoru.zhan <haoru.zhan@howbuy.com>
2015-10-09 19:13:58 +0800

    添加测试

---
+++ WorkFrame/WorkFrame/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  WorkFrame
+//
+//  Created by howbuy on 15/10/9.
+//  Copyright (c) 2015年 tiannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test : HBBaseTableViewController
+
+@end
---
+++ WorkFrame/WorkFrame/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  WorkFrame
+//
+//  Created by howbuy on 15/10/9.
+//  Copyright (c) 2015年 tiannu. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
